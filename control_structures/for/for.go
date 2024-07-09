50f4ca92e95e324f05805c0e07325e055e0ae815
victor_ferreira <vitorsantosferreira10@gmail.com>
2024-07-08 22:33:20 -0300

    feat: for

---
+++ control_structures/for/for.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 1
+
+	for i <= 10 { // não tem while em go, mas tem for
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i <= 20; i += 2 {
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Println("\nMisturando...")
+
+	for i = 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("Par")
+		} else {
+			fmt.Println("Impar")
+		}
+	}
+
+	for {
+		// laco infinito
+		fmt.Println("Laço infinito...")
+		time.Sleep(time.Second) // pausa de 1 segundo
+	}
+}
