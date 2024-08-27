4e12fa4461a23d0d05573eb3ba5d86cd8e3f0298
victor_ferreira <vitorsantosferreira10@gmail.com>
2024-08-26 22:47:31 -0300

    feat: learning about packages

---
+++ packages/usandolib/go.mod
@@ -0,0 +1,5 @@
+module goarea
+
+go 1.22.4
+
+require github.com/victor-ferreira95/goarea v0.0.0-20240827011531-ef78a59a2182 // indirect
---
+++ packages/usandolib/usandolib.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/victor-ferreira95/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+}
